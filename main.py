e6e16ad0cb8df9a1c07e27d1d4473d0defcd78b4
Aviral Mittal <aviralmittal0012@gmail.com>
2026-01-31 18:46:48 +0530

    Pipeline is completed

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ from networksecurity.entity.config_entity import (
     DataValidationConfig,
     DataTransformationConfig
 )
+
 import sys
 from networksecurity.components.data_transformation import DataTransformation
-
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 
 
 if __name__ == "__main__":
@@ -18,6 +20,7 @@ if __name__ == "__main__":
         logging.info("Starting training pipeline")
 
         training_pipeline_config = TrainingPipelineConfig()
+        
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
         logging.info("Initiating Data Ingestion")
@@ -31,12 +34,20 @@ if __name__ == "__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+        
         data_transformation_config=DataTransformationConfig(training_pipeline_config)
         logging.info("data Transformation started")
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+        
+        logging.info("Model Training started")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
         
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -17,6 +17,7 @@ class TrainingPipelineConfig:
         self.artifact_name = training_pipeline.ARTIFACT_DIR
         self.artifact_dir = os.path.join(self.artifact_name, timestamp)
         self.timestamp: str = timestamp
+        
 
 
 class DataIngestionConfig:
@@ -76,3 +77,18 @@ class DataTransformationConfig:
             training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
         self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
             training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
+        
+
+class ModelTrainerConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.model_trainer_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_TRAINER_DIR_NAME
+        )
+        self.trained_model_file_path: str = os.path.join(
+            self.model_trainer_dir, training_pipeline.MODEL_TRAINER_TRAINED_MODEL_DIR,
+            training_pipeline.MODEL_FILE_NAME
+        )
+        self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
+        self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_FITTING_UNDER_FITTING_THRESHOLD
+        
+        
